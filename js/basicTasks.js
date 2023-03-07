abd3d799267977fd9355c0fcc28997218f0a40e9
Aleksey <example@gmail.com>
2023-03-07 11:57:49 +0300

    'basicTasks'

---
+++ js/basicTasks.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// let x = 5; console.log( x++ ); // 5 Постфиксная форма возвращает сначала исходное значение // (++x) 6 Префиксная
+
+// [] + false - null + true; // NaN
+// console.log([] + false); // [] - пустая строка // 'false'
+// console.log([] + false - null); // NaN
+
+let y = 1;
+let x = y = 2; // Cправа налево
+console.log(x);
+
+console.log([ ] + 1 + 2); // '12'
+
+console.log('1'[0]); // 1
+
+console.log(2 && 1 && null && 0 && undefined); // null && Запинается на лжи
+
+console.log(!!( 1 && 2 )); // !! дают boolean true
+console.log((1 && 2)); // 2
+
+console.log(null || 2 && 3 || 4); // 3 || Запинается на да 
+
+const a = [1, 2, 3];
+const b = [1, 2, 3]; // a != b
+
+console.log( +"Infinity" ); // Infinity (type number) унарный плюс
+
+console.log("Ёжик" > "яблоко"); // false Uniicode А а Б б В в ...
+
+console.log(0 || "" || 2 || undefined || true || falsе);
--- js/script.js
+++ js/script.js
@@ -2,30 +2,50 @@
 
 "use strict";
 
-alert('hello!!!');
+// alert('hello!!!');
 
-const result = confirm("are you here?");
-console.log(result);
+// const result = confirm("are you here?");
+// console.log(result);
 
-const answer = +prompt("Вам есть 18?", "");
-console.log(answer + 5)
+// const answer = +prompt("Вам есть 18?", "");
+// console.log(answer + 5)
 
-const answers = [];
+// const answers = [];
 
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваша фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
 
-document.write(answers);
+// document.write(answers);
 
-console.log(typeof(answers))
+// console.log(typeof(answers))
 
-// Lesson 4 // Interpolation //
+// // Lesson 4 // Interpolation //
 
-const category = 'toys';
+// const category = 'toys';
 
-console.log(`https://someurl.com/${category}/5`);
+// console.log(`https://someurl.com/${category}/5`);
 
-const user = "Ivan"
-alert(`Привет, ${user}!`)
+// const user = "Ivan"
+// alert(`Привет, ${user}!`)
 
+
+function createCounter () {
+	let counter = 0;
+
+	const myFunction = function () {
+		counter += 1;
+		return counter;
+	}
+
+	return myFunction;
+}
+
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+
+console.log(c1, c2, c3);
+
+console.log(0 && 1);
